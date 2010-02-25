db06d25c2cc280df81e55fbbd8a143cdea4b1457
Mike Jewell <mjewell@gmail.com>
2010-02-25 22:33:03 +0000

    Tweaks for multilne

--- extractors/TagExtractor.py
+++ extractors/TagExtractor.py
@@ -5,7 +5,7 @@ class TagExtractor(Extractor):
 
 	def run(self, text):
 		results = dict()
-		group = re.findall(r"<([a-zA-Z0-9]+)\b", text)
+		group = re.findall(r"<([a-zA-Z0-9]+)\b", text, re.I|re.M)
 		for tag in group:
 			if results.has_key(tag):
 				results[tag] += 1
