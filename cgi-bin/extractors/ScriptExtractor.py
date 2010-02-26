2e51a728c7f3f7f56104660b1d8e5cc517980224
Mike Jewell <mjewell@gmail.com>
2010-02-26 16:46:54 +0000

    Tweak

--- cgi-bin/extractors/ScriptExtractor.py
+++ cgi-bin/extractors/ScriptExtractor.py
@@ -5,22 +5,22 @@ class ScriptExtractor(Extractor):
 
 	def run(self, text):
 		results = dict()
-		results['jsness'] = 0
+		results['js.ness'] = 0
 
 		if re.search(r'[\'"][^\'"]+prototype.js[\'"]', text, re.M|re.I):
-			results['jsness'] += 1
+			results['js.ness'] += 1
 			results['js.prototype'] = 1
 		
 		if re.search(r'[\'"][^\'"]+scriptaculous.js[\'"]', text, re.M|re.I):
-			results['jsness'] += 1
+			results['js.ness'] += 1
 			results['js.scriptaculous'] = 1
 		
 		if re.search(r'[\'"][^\'"]+mootools.js[\'"]', text, re.M|re.I):
-			results['jsness'] += 1
+			results['js.ness'] += 1
 			results['js.mootools'] = 1
 		
 		if re.search(r'[\'"][^\'"]+jquery.js[\'"]', text, re.M|re.I):
-			results['jsness'] += 1
+			results['js.ness'] += 1
 			results['js.jquery'] = 1
 
 		return results 
