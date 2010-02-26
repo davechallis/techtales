497cd8aff55a33f365af5621bf1980c23802452c
Mike Jewell <mjewell@gmail.com>
2010-02-26 16:16:11 +0000

    Fades

--- htdocs/js/core.js
+++ htdocs/js/core.js
@@ -12,14 +12,15 @@ function handleJSON(data, textStatus)
 
 function getSiteGraph()
 {
-	$("#graph").fadeOut();
-	$("#throbber").fadeIn();
-	var siteurl = $("#siteurl").val();
-	var tag1 = $("#tag1").val();
-	var tag2 = $("#tag2").val();
-	var tag3 = $("#tag3").val();
-	$.getJSON("/service/chart", {url:siteurl,fields:[tag1,tag2,tag3].join(",")}, handleJSON);
-
+	$("#graph").fadeOut(function()
+	{
+			$("#throbber").fadeIn();
+			var siteurl = $("#siteurl").val();
+			var tag1 = $("#tag1").val();
+			var tag2 = $("#tag2").val();
+			var tag3 = $("#tag3").val();
+			$.getJSON("/service/chart", {url:siteurl,fields:[tag1,tag2,tag3].join(",")}, handleJSON);
+	});
 }
 
 $(function() {
