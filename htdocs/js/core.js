7bc58664be1903812793c8854df01e0eb94b35e1
Mike Jewell <mjewell@gmail.com>
2010-02-26 15:26:08 +0000

    Lots of html/css tweaks

--- htdocs/js/core.js
+++ htdocs/js/core.js
@@ -1,9 +1,26 @@
 function handleJSON(data, textStatus)
 {
-	var img = $("<img />");
-	img.attr("src", data.url);
-	img.appendTo("#graphbox");
+	if(data)
+	{
+		var img = $("<img />");
+		img.attr("src", data.url);
+		img.appendTo("#graph");
+	}
 }
 
-$.getJSON("/service/chart", {url:"www.ecs.soton.ac.uk",field:"table"}, handleJSON);
+function getSiteGraph()
+{
+	var siteurl = $("#siteurl").val();
+	var tag1 = $("#tag1").val();
+	var tag2 = $("#tag2").val();
+	var tag3 = $("#tag3").val();
+	$.getJSON("/service/chart", {url:siteurl,field:tag1}, handleJSON);
+}
+
+$(function() {
+
+$("#submit").click(getSiteGraph);
+
+});
+
 
