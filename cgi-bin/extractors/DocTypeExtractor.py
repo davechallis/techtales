a430b3263106d34144b2d3c19387e608813c8e82
Mike Jewell <mjewell@gmail.com>
2010-02-26 12:08:36 +0000

    More useful results from doctype

--- cgi-bin/extractors/DocTypeExtractor.py
+++ cgi-bin/extractors/DocTypeExtractor.py
@@ -5,24 +5,122 @@ class DocTypeExtractor(Extractor):
 
 	def run(self, text):
 		results = dict()
+
+		root = None
+		fpi = None
+		uri = None
+
 		p2 = re.search(r'<!DOCTYPE\s+(\S+)\s+PUBLIC\s+"([^"]+?)"\s+"([^"]+?)">', text, re.M|re.I)
 		if p2:
-			results['root'] = p2.group(1)
-			results['fpi'] = p2.group(2)
-			results['uri'] = p2.group(3)
-			results['kind'] = 'public'
-		
+			fpi = p2.group(2)
+			uri = p2.group(3)
+
 		p1 = re.search(r'<!DOCTYPE\s+(\S+)\s+PUBLIC\s+"([^"]+)">', text, re.M|re.I)
 		if p1:
-			results['root'] = p1.group(1)
-			results['fpi'] = p1.group(2)
-	
+			root = p1.group(1)
+			fpi = p1.group(2)
+
+		if fpi:
+			if fpi == "-//W3C//DTD HTML 4.01//EN":
+				results['doctype.html401_strict'] = 1
+				results['doctype.html401'] = 1
+				results['doctype.html'] = 1
+			elif fpi == "-//W3C//DTD HTML 4.01 Transitional//EN":
+				results['doctype.html401_transitional'] = 1
+				results['doctype.html401'] = 1
+				results['doctype.html'] = 1
+			elif fpi == "-//W3C//DTD HTML 4.01 Frameset//EN":
+				results['doctype.html401_frames'] = 1
+				results['doctype.html401'] = 1
+				results['doctype.html'] = 1
+			elif fpi == "-//W3C//DTD XHTML 1.0 Strict//EN":
+				results['doctype.xhtml1_strict'] = 1
+				results['doctype.xhtml1'] = 1
+				results['doctype.xhtml'] = 1
+			elif fpi == "-//W3C//DTD XHTML 1.0 Transitional//EN":
+				results['doctype.xhtml1_transitional'] = 1
+				results['doctype.xhtml1'] = 1
+				results['doctype.xhtml'] = 1
+			elif fpi == "-//W3C//DTD XHTML 1.0 Frameset//EN":
+				results['doctype.xhtml1_frames'] = 1
+				results['doctype.xhtml1'] = 1
+				results['doctype.xhtml'] = 1
+			elif fpi == "-//W3C//DTD XHTML 1.1//EN":
+				results['doctype.xhtml11'] = 1
+				results['doctype.xhtml'] = 1
+			elif fpi == "-//W3C//DTD XHTML Basic 1.0//EN":
+				results['doctype.xhtml10_basic'] = 1
+				results['doctype.xhtml10'] = 1
+				results['doctype.xhtml'] = 1
+			elif fpi == "-//W3C//DTD XHTML Basic 1.1//EN":
+				results['doctype.xhtml11_basic'] = 1
+				results['doctype.xhtml11'] = 1
+				results['doctype.xhtml'] = 1
+			elif fpi == "-//WAPFORUM/DTD XHTML Mobile 1.0//EN":
+				results['doctype.mobile10'] = 1
+				results['doctype.mobile'] = 1
+				results['doctype.xhtml'] = 1
+			elif fpi == "-//WAPFORUM/DTD XHTML Mobile 1.1//EN":
+				results['doctype.mobile11'] = 1
+				results['doctype.mobile'] = 1
+				results['doctype.xhtml'] = 1
+			elif fpi == "-//WAPFORUM/DTD XHTML Mobile 1.2//EN":
+				results['doctype.mobile12'] = 1
+				results['doctype.mobile'] = 1
+				results['doctype.xhtml'] = 1
+
+		
 		s1 = re.search(r'<!DOCTYPE\s+(\S+)\s+SYSTEM\s+"(.+)">', text, re.M|re.I)
 		if s1:
-			results['root'] = s1.group(1)
-			results['uri'] = s1.group(2)
+			root = s1.group(1)
+			uri = s1.group(2)
+		
+		if uri:
+			if uri == "http://www.w3.org/TR/html4/strict.dtd":
+				results['doctype.html401_strict'] = 1
+				results['doctype.html'] = 1
+			elif uri == "http://www.w3.org/TR/html4/loose.dtd":
+				results['doctype.html401_transitional'] = 1
+				results['doctype.html'] = 1
+			elif uri == "http://www.w3.org/TR/html4/frameset.dtd":
+				results['doctype.html401_frames'] = 1
+				results['doctype.html'] = 1
+			elif uri == "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd":
+				results['doctype.xhtml1_strict'] = 1
+				results['doctype.xhtml'] = 1
+			elif uri == "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd":
+				results['doctype.xhtml1_transitional'] = 1
+				results['doctype.xhtml'] = 1
+			elif uri == "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd":
+				results['doctype.xhtml1_frames'] = 1
+				results['doctype.xhtml'] = 1
+			elif uri == "http://www.w3.org/TR/xhtml1/DTD/xhtml11.dtd":
+				results['doctype.xhtml11'] = 1
+				results['doctype.xhtml'] = 1
+			elif uri == "http://www.w3.org/TR/xhtml-basic/xhtml-basic10.dtd":
+				results['doctype.xhtml10_basic'] = 1
+				results['doctype.xhtml10'] = 1
+				results['doctype.xhtml'] = 1
+			elif uri == "http://www.w3.org/TR/xhtml-basic/xhtml-basic11.dtd":
+				results['doctype.xhtml10'] = 1
+				results['doctype.xhtml'] = 1
+			elif uri == "http://www.wapforum.org/DTD/xhtml-mobile10.dtd":
+				results['doctype.mobile10'] = 1
+				results['doctype.mobile'] = 1
+				results['doctype.xhtml'] = 1
+			elif uri == "http://www.openmobilealliance.org/tech/DTD/xhtml-mobile11.dtd":
+				results['doctype.mobile11'] = 1
+				results['doctype.mobile'] = 1
+				results['doctype.xhtml'] = 1
+			elif uri == "http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd":
+				results['doctype.mobile12'] = 1
+				results['doctype.mobile'] = 1
+				results['doctype.xhtml'] = 1
+
 
 		d1 = re.search(r'<!DOCTYPE\s+(\S+)>', text, re.M|re.I)
 		if d1:
-			results['root'] = d1.group(1)
+			root = d1.group(1)
+			results['doctype.html5'] = 1
+
 		return results 
--- cgi-bin/techtales.py
+++ cgi-bin/techtales.py
@@ -6,8 +6,16 @@ import os.path
 
 #cherrypy.config['log.error_file'] = '/home/www/sites/techtales/logs/py_error.log'
 
+class Stats:
+	def default(self, uri=None):
+		if not uri:
+		    return dumps({'status':'error', 'message':'Need URI'})
+		return dumps({})
+
+	default.exposed = True
+
 class Main:
-	pass
+	stats = Stats()
 #
 # These methods take care of running the application via either mod_python or
 # stand-alone using the built-in CherryPy server.
