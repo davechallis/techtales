a36d4f0469d2529211801f533b56d88f51244c28
Mike Jewell <mjewell@gmail.com>
2010-02-26 15:45:37 +0000

    Mutlifield

--- htdocs/js/core.js
+++ htdocs/js/core.js
@@ -16,7 +16,7 @@ function getSiteGraph()
 	var tag1 = $("#tag1").val();
 	var tag2 = $("#tag2").val();
 	var tag3 = $("#tag3").val();
-	$.getJSON("/service/chart", {url:siteurl,field:tag1}, handleJSON);
+	$.getJSON("/service/chart", {url:siteurl,fields:[tag1,tag2,tag3]}, handleJSON);
 
 	$("#graph").fadeOut();
 }
