234c031bdff70b1936918c87094d54add25ba489
Mike Jewell <mjewell@gmail.com>
2010-02-26 11:05:21 +0000

    Lots of stuffs.

---
+++ cgi-bin/techtales.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import cherrypy
+from simplejson import dumps 
+import os.path
+
+#cherrypy.config['log.error_file'] = '/home/www/sites/techtales/logs/py_error.log'
+
+class Main:
+	pass
+#
+# These methods take care of running the application via either mod_python or
+# stand-alone using the built-in CherryPy server.
+#
+def start_modpython():
+    cherrypy.engine.SIGHUP = None
+    cherrypy.engine.SIGTERM = None
+    cherrypy.tree.mount(Main(), '/service')
+    cherrypy.engine.start(blocking=False)
+
+def start_standalone():
+    cherrypy.quickstart(Main(), '/service')
+
+#
+# If we're not being imported, it means we should be running stand-alone.
+#
+if __name__ == '__main__':
+	cherrypy.config['log.error_file'] = 'logs/py_error.log'
+	start_standalone()
+
